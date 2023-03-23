01fa879002456744f5085bea58569134723456d1
PHÙ VĨNH HÙNG - ITC - App - IT Trainee <hung.phu@mservice.com.vn>
2023-03-24 02:55:04 +0700

    chore: update

--- build/index.js
+++ build/index.js
@@ -86,30 +86,8 @@ app.get("/", function (req, res) {
     }
 });
 app.post("/", function (req, res) {
-    // Parse the query params
-    var mode = req.query["hub.mode"];
-    var token = req.query["hub.verify_token"];
-    var challenge = req.query["hub.challenge"];
-    console.log("mode: ", mode);
-    console.log("token: ", token);
-    console.log("challenge: ", challenge);
-    console.log(req.body);
-    // Check if a token and mode is in the query string of the request
-    if (mode && token) {
-        // Check the mode and token sent is correct
-        if (mode === "subscribe" && token === token) {
-            // Respond with the challenge token from the request
-            console.log("WEBHOOK_VERIFIED");
-            res.status(200).send(challenge);
-        }
-        else {
-            // Respond with '403 Forbidden' if verify tokens do not match
-            res.sendStatus(403);
-        }
-    }
-    else {
-        res.sendStatus(403);
-    }
+    var body = req.body;
+    console.log(body.entry[0].messaging);
 });
 app.listen(port, function () {
     console.log("Example app listening on port ".concat(port));
